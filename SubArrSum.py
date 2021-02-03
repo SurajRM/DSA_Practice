401eaa848c74e3c323127e1430095fd8a9f23698
Suraj Mukkannavar <surajrm.is19@rvce.edu.in>
2021-02-03 09:00:59 +0530

    First Commit

--- SubArrSum.py
+++ SubArrSum.py
@@ -1,1 +1,17 @@
+def subArraySum(arr,n,s):
+    for k in range(n):
+        for i in range(n - k + 1):
+            current_sum = 0
+            sub_arr = []
+            for j in range(k):
+                sub_arr.append(arr[i+j])
+                current_sum = current_sum + int(arr[i + j])
+                if current_sum == s:
+                    return sub_arr
 
+
+
+arr = input("Enter array elements: ").split()
+n = len(arr)
+s = int(input("\nSum of expected subarray: "))
+print(subArraySum(arr,n,s))
