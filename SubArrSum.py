dd9a1ed037dce0bbe4fdd6be93a30f10ec1ea8a0
Suraj Mukkannavar <surajrm.is19@rvce.edu.in>
2021-02-03 11:58:24 +0530

    Second Commit

--- SubArrSum.py
+++ SubArrSum.py
@@ -1,16 +1,13 @@
-#Data Structur: Array
-#Print sub array with sum of elements as the given sum
-
 def subArraySum(arr,n,s):
     for k in range(n):
         for i in range(n - k + 1):
             current_sum = 0
             sub_arr = []
             for j in range(k):
-                sub_arr.append(arr[i+j])
+                sub_arr.append(i+j+1)
                 current_sum = current_sum + int(arr[i + j])
                 if current_sum == s:
-                    return sub_arr
+                    return [sub_arr[0],sub_arr[-1]]
 
 
 
